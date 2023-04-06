d8dedaeeb1e5289b293f7ed54537cda9f5fc385e
cdgrody <cdgrody@gmail.com>
2023-04-05 22:34:55 -0400

    main calendar page setup and excess components are removed

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -2,27 +2,18 @@ import './App.css';
 import { useState } from 'react'
 import { Routes, Route } from 'react-router-dom'
 import { getUser } from '../../utilities/users-service'
-import AuthPage from '../AuthPage/AuthPage';
-import NewOrderPage from '../NewOrderPage/NewOrderPage';
-import OrderHistoryPage from '../OrderHistoryPage/OrderHistoryPage';
-import NavBar from '../../components/NavBar/NavBar'
+import Calendar from '../Calendar/Calendar'
 
 export default function App() {
-  const [ user, setUser ] = useState(getUser())
 
   return (
     <main className="App">
       {
-        user ?
         <>
-          <NavBar user={user} setUser={setUser} />
           <Routes>
-            <Route path="/orders/new" element={<NewOrderPage />} />
-            <Route path="/orders" element={<OrderHistoryPage />} />
+            <Route path="/" element={<Calendar />} />
           </Routes>
         </>
-        :
-        <AuthPage setUser={setUser} />
       }
     </main>
   );
