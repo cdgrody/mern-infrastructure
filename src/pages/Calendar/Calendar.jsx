b3d16aa9bd4c7ca8a43a08ed3f5e971a099e5ae4
cdgrody <cdgrody@gmail.com>
2023-04-05 23:18:59 -0400

    creates component for day labels ie Holiday, Work, etc...

--- src/pages/Calendar/Calendar.jsx
+++ src/pages/Calendar/Calendar.jsx
@@ -1,4 +1,5 @@
 import DayCard from "../../components/DayCard/DayCard";
+import DayLabel from "../../components/DayLabel/DayLabel";
 import "./Calendar.css";
 
 export default function Calendar() {
@@ -28,6 +29,7 @@ export default function Calendar() {
 
   // The following creates an array of numbers from [1..28]
   const dates = Array.from({ length: 28 }, (x, i) => i + 1);
+  const dayTypes = ['Holiday', 'Work', 'Errands', 'Sick']
 
   // Creates an Array of Dates
   const dateObjs = dates.map(
@@ -39,6 +41,9 @@ export default function Calendar() {
       <div className="App">
         <div className="react-calendar">
           <h1>React Calendar</h1>
+        </div>
+        <div className="day-label">
+            <DayLabel />
         </div>
         <div className="day-card-list">
           {dateObjs.map((dateObj, idx) => (
