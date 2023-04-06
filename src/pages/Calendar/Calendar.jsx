bc003b84263040526fe4e3cf897e786cf92ca54f
cdgrody <cdgrody@gmail.com>
2023-04-05 22:51:23 -0400

    an array of date objects is created with the weekday and key for each date

--- src/pages/Calendar/Calendar.jsx
+++ src/pages/Calendar/Calendar.jsx
@@ -1,6 +1,41 @@
 export default function Calendar() {
-    return (
-        <>hello
-        </>
-    )
+  const days = [
+    {
+      name: "Sunday",
+    },
+    {
+      name: "Monday",
+    },
+    {
+      name: "Tuesday",
+    },
+    {
+      name: "Wednesday",
+    },
+    {
+      name: "Thursday",
+    },
+    {
+      name: "Friday",
+    },
+    {
+      name: "Saturday",
+    },
+  ];
+
+  // The following creates an array of numbers from [1..28]
+  const dates = Array.from({ length: 28 }, (x, i) => i + 1);
+
+  // Creates an Array of Dates
+  const dateObjs = dates.map(
+    (date, key) => (date = { day: days[key % 7].name, idx: key + 1 })
+  );
+
+  return (
+    <>
+      <div className="App">
+        <h1>React Calendar</h1>
+      </div>
+    </>
+  );
 }
